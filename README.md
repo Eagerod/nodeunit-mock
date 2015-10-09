d6b3fef43e1f8f793b1416219a2a5efca8754302
Aleem Haji <hajial@gmail.com>
2015-10-09 15:33:58 -0400

    Initial commit -- Copied out npm-package-base, and replaced some text here and there.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Nodeunit Mocks
---
+++ tests/test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var mock = require("..");
+
+module.exports = {
+    "Test": function(test) {
+        console.log(test);
+        console.log(mock);
+    }
+};
