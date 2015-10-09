87a66f9ed51045c43296bc14641a46c54f3e9f0d
Aleem Haji <hajial@gmail.com>
2015-10-09 15:46:51 -0400

    Added in basic framework with a couple tests.

--- tests/test.js
+++ tests/test.js
@@ -2,9 +2,39 @@
 
 var mock = require("..");
 
+// This is my test application.
+var app = {
+    regularFunction: function() {
+        return 1;
+    }
+};
+
 module.exports = {
-    "Test": function(test) {
-        console.log(test);
-        console.log(mock);
+    "Regular function mock pass through": function(test) {
+        test.expect(3);
+        var rf = mock(test, app, "regularFunction");
+        test.equal(app.regularFunction(), 1);
+        test.equal(rf.callCount, 1);
+        test.deepEqual(rf.callArguments, [[]]);
+        test.done();
+    },
+    "Regular function mock alternate return": function(test) {
+        test.expect(3);
+        function replacementFunction() {
+            return 2;
+        }
+        var rf = mock(test, app, "regularFunction", replacementFunction);
+        test.equal(app.regularFunction(), 2);
+        test.equal(rf.callCount, 1);
+        test.deepEqual(rf.callArguments, [[]]);
+        test.done();
+    },
+    "Regular function no function call": function(test) {
+        test.expect(3);
+        var rf = mock(test, app, "regularFunction", null);
+        test.equal(app.regularFunction(), undefined);
+        test.equal(rf.callCount, 1);
+        test.deepEqual(rf.callArguments, [[]]);
+        test.done();
     }
 };
