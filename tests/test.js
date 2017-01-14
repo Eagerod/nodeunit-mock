1cfd668ea9d005856b5e549ec6e2b2078f192dce
Aleem Haji <hajial@gmail.com>
2017-01-13 23:18:44 -0500

    Quick little check to make sure that if you pass undefined into the test, it fails in a more sane way than previously.

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,8 @@
 "use strict";
 
 module.exports = {
-   "Property Tests": require("./mockpropertytests"),
-   "Function Mock Tests": require("./functionmocktests"),
-   "Prototype Tests": require("./prototypetests")
+    "Sanity Tests": require("./sanitytests"),
+    "Property Tests": require("./mockpropertytests"),
+    "Function Mock Tests": require("./functionmocktests"),
+    "Prototype Tests": require("./prototypetests")
 };
